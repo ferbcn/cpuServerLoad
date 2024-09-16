e9cd9e4f4b235e74fea69b3753edf4f5d676dca7
ferbcn <ferbcn@pm.me>
2024-09-16 21:16:43 +0200

    authentication access token env

--- app/main.py
+++ app/main.py
@@ -1,14 +1,29 @@
+import os
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.responses import JSONResponse
 import psutil
 
-
 app = FastAPI(title='WebsocketAPI')
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Getting access token from environment variable
+access_token = os.getenv("SERVER_TOKEN")
+
+
+def verify_token(token: str = Depends(oauth2_scheme)):
+    if not token == access_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or missing token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
 
 @app.get("/api-stats", response_class=JSONResponse)
-async def stats():
+async def stats(token: str = Depends(verify_token)):
     cpu_percent = psutil.cpu_percent(interval=1)
     mem_percent = psutil.virtual_memory().percent
     data = {"cpu": cpu_percent, "mem": mem_percent}
